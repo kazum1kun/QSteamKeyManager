9b8cd8cfcd3967bd57c0d42335c510346c3746ce
Xuanli Lin <l19980623@gmail.com>
2018-12-28 17:05:56 -0600

    Tweaked UI for table and minor code refactoring in MainWindow

--- package/MainWindow.py
+++ package/MainWindow.py
@@ -14,28 +14,48 @@ class MainWindow(QMainWindow, Ui_main_window):
         # Load UI layout defined in the ui file
         self.setupUi(self)
 
+        # Table view and model
+        self.setup_model()
+        self.setup_view()
+
+        # Top toolbar
+        self.setup_language_grouping()
+
+        # Showtime!
+        self.show()
+
+    # Set up the model in the table and link it to the view
+    def setup_model(self):
+        # Set up a SQL model
+        model = QSqlTableModel(self, DBO.get_or_create_db())
+        model.setTable(ENV.game_table_name)
+        model.setEditStrategy(QSqlTableModel.OnRowChange)
+
+        # Customize the name of headers
+        model.setHeaderData(1, Qt.Horizontal, 'Game')
+        model.setHeaderData(2, Qt.Horizontal, 'Key/URL')
+        model.setHeaderData(3, Qt.Horizontal, 'Notes')
+        model.select()
+
+        # Link table model to the table view
+        self.table_view_content.setModel(model)
+
+    # Tweak some setting in the view
+    def setup_view(self):
+        # Set the size of each columns
+        self.table_view_content.setColumnWidth(1, 250)
+        self.table_view_content.setColumnWidth(2, 250)
+        self.table_view_content.setColumnWidth(3, 100)
+
+        # Hide some columns: id, category, and tag
+        self.table_view_content.hideColumn(0)
+        self.table_view_content.hideColumn(4)
+        self.table_view_content.hideColumn(5)
+
+    # Set up grouping in the language selector
+    def setup_language_grouping(self):
         # Group language selector actions
         language_group = QActionGroup(self)
         self.action_english.setActionGroup(language_group)
         self.action_chinese_simplified.setActionGroup(language_group)
         self.action_japanese.setActionGroup(language_group)
-
-        # Set up a SQL model
-        self.model = QSqlTableModel(self, DBO.get_or_create_db())
-        self.model.setTable(ENV.game_table_name)
-        self.model.setEditStrategy(QSqlTableModel.OnRowChange)
-
-        # Customize the name of headers
-        self.model.setHeaderData(0, Qt.Horizontal, 'Game')
-        self.model.setHeaderData(1, Qt.Horizontal, 'Key/URL')
-        self.model.setHeaderData(2, Qt.Horizontal, 'Notes')
-        print('{}, {}'.format(self.model.columnCount(), self.model.rowCount()))
-
-        self.table_view_content.setModel(self.model)
-
-        # Set the size of each columns
-        self.table_view_content.setColumnWidth(0, 100)
-        self.table_view_content.setColumnWidth(1, 100)
-        self.table_view_content.setColumnWidth(2, 100)
-
-        self.show()
