9e98b790df85b33bc3d36f436cd73b868c7b7a5b
Xuanli Lin <l19980623@gmail.com>
2019-05-14 17:35:48 +0800

    Implemented copying key to the clipboard

--- package/TableContextMenu.py
+++ package/TableContextMenu.py
@@ -1,4 +1,5 @@
 # A context menu for the table
+import pyperclip
 from PyQt5.QtWidgets import QMenu, QAction
 from package.DBO import DBO
 
@@ -8,16 +9,18 @@ class TableContextMenu:
     def show(parent, pos):
         # Define a menu and some actions
         menu = QMenu(parent)
+        action_copy_key = QAction("Copy Key", parent)
         action_delete = QAction("Delete", parent)
-        action_copy = QAction("Copy Key", parent)
-        menu.addAction(action_copy)
+        menu.addAction(action_copy_key)
         menu.addAction(action_delete)
 
         # Present the menu on the mouse click point and perform accordingly
         action = menu.exec_(parent.table_view_content.viewport().mapToGlobal(pos))
 
-        if action == action_copy:
-            print(parent.table_view_content.selectionModel().currentIndex().data)
+        # Copy the key on the row currently selected by the user
+        if action == action_copy_key:
+            current_index = parent.table_view_content.selectionModel().currentIndex()
+            pyperclip.copy(current_index.sibling(current_index.row(), 2).data())
 
         # Delete row(s) selected by the user
         if action == action_delete:
