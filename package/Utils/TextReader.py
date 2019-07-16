389b6bb5eed7ab5dfd00c205cfcbd2c378d8214a
Xuanli Lin <l19980623@gmail.com>
2019-07-16 15:14:37 +0800

    basic implementation of a file reader

--- package/Utils/TextReader.py
+++ package/Utils/TextReader.py
@@ -1,3 +1,8 @@
+import re
+
+from package.ENV import ENV
+
+
 class TextReader:
     """Support for reading old Java SKM file format"""
 
@@ -5,10 +10,7 @@ class TextReader:
     def read(input_file, delimiter):
         """Read the input file line by line and try to extract key/game/notes info"""
 
-        # Set up an list to store results
-        game_key_list = []
-
-        # Now comes the interesting part: since the pre-QSKM text storage format is chaotic at best,
+        # Here's the interesting part: since the pre-QSKM text storage format is chaotic at best,
         # there is no way to guarantee if the lines are properly segregated, nor the positions of
         # tokens. Here we assume that the lines are delimited by semicolons (;) (or the user-supplied
         # delimiter).
@@ -21,26 +23,68 @@ class TextReader:
 
             # Read a few lines and determine if (1) the delimiter is valid, and (2) the arrangement
             # of the elements.
-            for i in range(0, 9):
+            for i in range(0, len(file_content)):
                 # Skip empty lines
                 if not file_content[i].strip():
                     tokens = file_content[i].split(delimiter)
-                    # If it's smaller than 2, definitely it's not the right separator...
-                    if len(tokens) < 2:
-                        continue
-                    if len(tokens) == 2:
-                        TextReader.parse_two_cols(file_content)
-                        break
-                    if len(tokens) == 3:
-                        TextReader.parse_three_cols(file_content)
-                        break
+
+                    # Parse the file if it finds a line containing 2 or 3 entries
+                    if len(tokens) == 2 or len(tokens) == 3:
+                        col_key = TextReader.determine_column_arrangement(file_content, delimiter, i)
+                        return TextReader.parse_file(file_content, delimiter, i, col_key)
+                    # If it's smaller than 2 or greater than three, definitely it's not the right separator...
                     else:
                         continue
+        # TODO: ask user what to do if none of the line seems to be working
+        return []
 
     @staticmethod
-    def parse_two_cols(content):
-        pass
+    def determine_column_arrangement(content, delimiter, first_row):
+        """Here we a line and figure out which column are the key/game on"""
+        # Only col 1 and 2 will be tested. Col 3 (if exists) will always be regarded as comments
+        tokens = content[first_row].split(delimiter)
+        col_key = -1
+
+        # Check each cols against predefined regex
+        if (re.match(ENV.steam_key_regex, tokens[0]) or
+                re.match(ENV.gog_key_regex, tokens[0]) or
+                re.match(ENV.hb_gift_regex, tokens[0]) or
+                re.match(ENV.itch_key_regex, tokens[0]) or
+                re.match(ENV.origin_key_regex, tokens[0]) or
+                re.match(ENV.uplay_key_regex, tokens[0])):
+            col_key = 0
+        elif (re.match(ENV.steam_key_regex, tokens[1]) or
+              re.match(ENV.gog_key_regex, tokens[1]) or
+              re.match(ENV.hb_gift_regex, tokens[1]) or
+              re.match(ENV.itch_key_regex, tokens[1]) or
+              re.match(ENV.origin_key_regex, tokens[1]) or
+              re.match(ENV.uplay_key_regex, tokens[1])):
+            col_key = 1
+        # TODO If none matches, ask user which col is the key resided
+
+        return col_key
 
     @staticmethod
-    def parse_three_cols(content):
-        pass
+    def parse_file(content, delimiter, start, col_key):
+        """Here we do actual file parsing."""
+        # Set up an list to store results
+        game_key_list = []
+
+        # Set the game column
+        if col_key == 1:
+            col_game = 0
+        else:
+            col_game = 1
+
+        for i in range(start, len(content)):
+            tokens = content[i].split(delimiter)
+
+            if len(tokens) >= 2:
+                game_entry = {"game": tokens[col_game],
+                              "key": tokens[col_key]}
+                if len(tokens) == 3:
+                    game_entry["notes"] = tokens[2]
+
+                game_key_list.append(game_entry)
+
+        return game_key_list
